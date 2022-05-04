496c8bef84e75cb05d0426613aa2121f04f2a2ac
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-05-04 08:39:45 -0400

    added hw start up

--- models/Category.js
+++ models/Category.js
@@ -6,9 +6,19 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
     // define columns
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: false,
+    autoIncrement: true,
   },
-  {
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+},
+},
+{
     sequelize,
     timestamps: false,
     freezeTableName: true,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: "category_id",
+  onDelete: "CASADE",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+})
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongToMany(Tag, {
+
+})
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongToMany(Products, {
+  
+})
 
 module.exports = {
   Product,
