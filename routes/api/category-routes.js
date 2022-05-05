e9a494f2ae58021314d993431225f11fd5a3f8db
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-05-05 07:00:36 -0400

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,7 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   Category.findAll({
-    include: [products]
+    include: [Product]
   })
   .then((categories) => res.json(categories))
   .catch((err) => res.status(500).json(err));
@@ -19,7 +19,7 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    include: [products]
+    include: [Product]
   })
   .then((categories) => res.json(categories))
   .catch(err => {
@@ -31,14 +31,50 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+
+  })
+  .then((categories) => res.json(categories))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+
+  },
+  {
+  where: {
+    id: req.params.id
+  }
+})
+  .then((categories) => res.json({message: 'Changed a category'}))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy(
+  {
+  where: {
+    id: req.params.id
+  }
+})
+  .then((categories) => res.json({message: 'category destroyed'}))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+
+  })
 });
 
 module.exports = router;
